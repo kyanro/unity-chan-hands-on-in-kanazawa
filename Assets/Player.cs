2282e819c7272811dbf73910bdc23ead1df10dd8
kyanro <gw.fnct+github-pub@gmail.com>
2014-12-14 16:10:29 +0900

    Playerクラス、Animation のリファクタ

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,26 +3,25 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
-
-		public float speed = 6;
-
-		// Use this for initialization
-		void Start ()
-		{
-
 	
-		}
+	public float speed = 6;
 	
-		// Update is called once per frame
-		void Update ()
-		{
-				rigidbody.MovePosition (transform.position + transform.forward * Time.deltaTime * speed);
-				if (Input.GetButtonDown ("Fire1")) {
-						GetComponent<Animator> ().SetTrigger ("JUMP");
-				}
-				if (Input.GetButtonDown ("Fire2")) {
-						GetComponent<Animator> ().SetTrigger ("SLIDE");
-				}
-
+	// Use this for initialization
+	void Start ()
+	{
+				
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		rigidbody.MovePosition (transform.position + transform.forward * Time.deltaTime * speed);
+		if (Input.GetButtonDown ("Fire1")) {
+			GetComponent<Animator> ().SetTrigger ("JUMP");
 		}
+		if (Input.GetButtonDown ("Fire2")) {
+			GetComponent<Animator> ().SetTrigger ("SLIDE");
+		}
+		
+	}
 }

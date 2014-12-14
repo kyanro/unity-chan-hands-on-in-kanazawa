747a0f8672923ff3cf9d1c5f671b5cf61e12e2b3
kyanro <gw.fnct+github-pub@gmail.com>
2014-12-14 16:39:06 +0900

    障害物でユニティちゃんがやられるように設定。まだ絶対こえれないよ！

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,24 +4,30 @@ using System.Collections;
 public class Player : MonoBehaviour
 {
 	
-	public float speed = 6;
+		public float speed = 6;
 	
-	// Use this for initialization
-	void Start ()
-	{
+		// Use this for initialization
+		void Start ()
+		{
 				
-	}
+		}
 	
-	// Update is called once per frame
-	void Update ()
-	{
-		rigidbody.MovePosition (transform.position + transform.forward * Time.deltaTime * speed);
-		if (Input.GetButtonDown ("Fire1")) {
-			GetComponent<Animator> ().SetTrigger ("JUMP");
+		// Update is called once per frame
+		void Update ()
+		{
+				rigidbody.MovePosition (transform.position + transform.forward * Time.deltaTime * speed);
+				if (Input.GetButtonDown ("Fire1")) {
+						GetComponent<Animator> ().SetTrigger ("JUMP");
+				}
+				if (Input.GetButtonDown ("Fire2")) {
+						GetComponent<Animator> ().SetTrigger ("SLIDE");
+				}
 		}
-		if (Input.GetButtonDown ("Fire2")) {
-			GetComponent<Animator> ().SetTrigger ("SLIDE");
+
+	void OnTriggerEnter (Collider collider)
+		     {
+				GetComponent<Animator> ().SetTrigger ("DAMAGED");
+				speed = 0;
 		}
-		
-	}
+
 }

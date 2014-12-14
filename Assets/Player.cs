673017642669740d14890ae7dc1e676e37b20396
kyanro <gw.fnct+github-pub@gmail.com>
2014-12-14 15:55:13 +0900

    Unity chan が走るスクリプトを追加

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+		public float speed = 6;
+
+		// Use this for initialization
+		void Start ()
+		{
+
 	
-	}
+		}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+		// Update is called once per frame
+		void Update ()
+		{
+		rigidbody.MovePosition ( transform.position + transform.forward * Time.deltaTime * speed);
+		}
 }

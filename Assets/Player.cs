4e365dcda846bd759a68cb5d50d4164ad307e390
kyanro <gw.fnct+github@gmail.com>
2014-12-17 06:12:47 +0900

    スライディング、ジャンプ中に正しくハードルと衝突するようにする

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -27,12 +27,36 @@ public class Player : MonoBehaviour
 		void OnTriggerEnter (Collider collider)
 		{
 				var stateInfo = GetComponent<Animator> ().GetCurrentAnimatorStateInfo (0);
-				bool isRun = stateInfo.IsName ("Base Layer.RUN00_F");
-
-				if (isRun) {
+				
+				if (TakesDamage (stateInfo, collider)) {
 						GetComponent<Animator> ().SetTrigger ("DAMAGED");
 						speed = 0;
 				}
+		}
+
+		private bool TakesDamage (AnimatorStateInfo state, Collider collider)
+		{
+				bool collidesHigh = collider.CompareTag ("High");
+				bool collidesLow = collider.CompareTag ("Low");
+
+				bool isRun = state.IsName ("Base Layer.RUN00_F");
+				bool isJump = state.IsName ("Base Layer.JUMP00");
+				bool isSlide = state.IsName ("Base Layer.SLIDE00");
+
+				if (isRun && (collidesHigh || collidesLow)) {
+						return true;
+				}
+
+				if (isJump && collidesHigh) {
+						return true;
+				}
+
+				if (isSlide && collidesLow) {
+						return true;
+				}
+
+				return false;
+
 		}
 
 }
